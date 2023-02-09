31e3b9e4f396b89ff87cc884af4b1a61be0d0528
estresadinha <120022356+estresadinha@users.noreply.github.com>
2023-02-09 11:52:50 -0300

    Add files via upload

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Web;
 
 namespace calculadora
 {
@@ -13,60 +13,69 @@ namespace calculadora
         {
             double valor1 = 0.0;
             double valor2 = 0.0;
-            string opc ="";
-            
+            /*string opc ="";*/
 
-            Console.WriteLine("digite o primeiro valor: ");
-            valor1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("digite o segundo valor ");
-            valor2 = Convert.ToDouble(Console.ReadLine());
+            string opcao = "";
 
-            //Console.WriteLine("Qual operação que fazer? ");
-           //Console.WriteLine(" 1- soma ");
-            //Console.WriteLine(" 2 - subtração ");
-          //Console.WriteLine(" 3 - divisão ");
-           // Console.WriteLine(" 4 - multiplicação ");
-           
-            Console.WriteLine(" digite sua opção");
-            opc = Console.ReadLine();
-            if (opc == "1")
+            while (opcao != "parar")
             {
-                Console.WriteLine("o resultado é " + (valor1 + valor2));
-                Console.WriteLine();
-                
-            }
-            if (opc == "2")
-            {
-                Console.WriteLine("o resultado é " + (valor1 - valor2));
-                Console.WriteLine();
-            }
-            if (opc == "3")
-            {
-               
-                Console.WriteLine("o resultado é " + (valor1 * valor2));
-                Console.WriteLine();
 
-                
 
-            }
-            if (opc == "4")
-            {
-                if (valor1 >= valor2)
+
+                Console.WriteLine("digite o primeiro valor: ");
+                valor1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("digite o segundo valor ");
+                valor2 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Qual operação que fazer? ");
+                Console.WriteLine(" 1- soma ");
+                Console.WriteLine(" 2 - subtração ");
+                Console.WriteLine(" 3 - divisão ");
+                Console.WriteLine(" 4 - multiplicação ");
+
+
+                Console.WriteLine(" digite sua opção");
+                switch (Console.ReadLine())
                 {
 
-                    Console.WriteLine("o resultado é " + (valor1 / valor2));
+
+                    case "1":
+                        Console.WriteLine("o resultado é " + (valor1 + valor2));
+                        break;
+
+                    case "2":
+                        Console.WriteLine("o resultado é " + (valor1 - valor2));
+                        break;
+
+                    case "3":
+                        Console.WriteLine("o resultado é " + (valor1 / valor2));
+                        break;
+
+                    case "4":
+                        if (valor1 >= valor2)
+                        {
+                            Console.WriteLine("o resultado é " + (valor1 * valor2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalido ");
+                        }
+                        break;
+
+
+
+
                 }
+                Console.WriteLine("quer continuar ");
+                
 
-
-                else
-                {
-                    Console.WriteLine("divisão invalida ");
-                }
-
+                opcao = Console.ReadLine();
             }
-                
-                
+             
+
                 
                  
                     
